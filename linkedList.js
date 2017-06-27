bd2064ac49ae3c1aa5de231de88c3354edb873b5
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-27 11:24:08 -1000

    add overall module framework

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,36 @@
  */
 function linkedListGenerator(){
 
+  function getHead(){
+
+  }
+
+  function getTail(){
+
+  }
+
+  function add(){
+
+  }
+
+  function get(){
+
+  }
+
+  function remove(){
+
+  }
+
+  function insert(){
+
+  }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    get: get,
+    remove: remove,
+    insert: insert
+  };
 }
